132941e12713b7fd0fc48b82b7f23a4c29bc78e8
viniciusoliveiras <vinitag190@gmail.com>
2021-03-29 14:25:09 -0300

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,10 @@
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={`${commonStyles.container} ${styles.header}`}>
+      <img src="/Logo.png" alt="logo" />
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,61 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import Head from 'next/head';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
-}
-
-interface PostPagination {
-  next_page: string;
-  results: Post[];
-}
-
-interface HomeProps {
-  postsPagination: PostPagination;
-}
-
-// export default function Home() {
-//   // TODO
+// interface Post {
+//   uid?: string;
+//   first_publication_date: string | null;
+//   data: {
+//     title: string;
+//     subtitle: string;
+//     author: string;
+//   };
 // }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+// interface PostPagination {
+//   next_page: string;
+//   results: Post[];
+// }
 
-//   // TODO
-// };
+// interface HomeProps {
+//   postsPagination: PostPagination;
+// }
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>home | blog</title>
+      </Head>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    { fetch: ['publication.title', 'publication.content'], pageSize: 20 }
+  );
+
+  // console.log(JSON.stringify(postsResponse, null, 2));
+
+  // const posts = postsResponse.results.map(post => {
+  //   return {
+  //     slug: post.uid,
+  //     title: post.data.title,
+  //   };
+  // });
+
+  return {
+    props: {},
+  };
+};
